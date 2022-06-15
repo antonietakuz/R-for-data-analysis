f33a2808810b4fa6338a9f1cecdda95bbd31e416
Anto <105023719+kuzantonieta@users.noreply.github.com>
2022-06-15 09:54:41 -0300

    Add files via upload

---
+++ Mi Primer Script.R
@@ -0,0 +1,32 @@
+2+3
+#3+7
+#####################
+#TITULO
+##################
+
+
+504*1000 # esta la formula de la bomba atomica
+37-9
+2**9
+
+geom_sf(data = poblacion,aes(fill=personas))
+
+library(tidyverse)
+install.packages("tidyverse")
+
+?tidyverse
+
+?sum()
+
+## Pass a vector to sum, and it will add the elements together.
+sum(1:5)
+
+## Pass several numbers to sum, and it also adds the elements.
+sum(1, 2, 3, 4, 5)
+
+
+
+
+
+
+
